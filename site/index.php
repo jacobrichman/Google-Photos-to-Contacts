898849a31f85c2eee4c77fa0e8dcd6f07f261f88
Jacob Richman <jarichman43@gmail.com>
2019-01-04 03:51:59 -0500

    extension submitted

---
+++ site/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-58617131-13"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-58617131-13');
+  </script>
+  <title>Google Photos to Contacts</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-teal.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+<body>
+
+<!-- Header -->
+<header class="w3-container w3-theme w3-padding" id="myHeader">
+  <div class="w3-center">
+    <h1>Google Photos to Contacts</h1>
+    <h4 class="w3-animate-bottom">Made by Jacob Richman</h4>
+  </div>
+</header>
+
+<div class="w3-row-padding w3-center w3-margin-top">
+<div class="w3-third">
+  <div class="w3-card w3-container" style="min-height:460px">
+  <h3>Google Photos People</h3><br>
+  <i class="fa fa-picture-o w3-margin-bottom w3-text-theme" style="font-size:120px"></i>
+  <p>1) In Google Photos,<br>
+    go to the "People" feature<br>
+    and assign names to<br>
+    each individual</p>
+    <a href="https://photos.google.com/people" target="_blank"><img src="https://cdn.worldvectorlogo.com/logos/google-photos.svg" width="70"></img><b> Google Photos</b></a>
+
+  </div>
+</div>
+
+<div class="w3-third">
+  <div class="w3-card w3-container" style="min-height:460px">
+  <h3>Import</h3><br>
+  <i class="fa fa-arrow-right w3-margin-bottom w3-text-theme" style="font-size:120px"></i>
+    <p>2) Install <a href="https://chrome.google.com/webstore/detail/pojngholjbkbkblepogbkphpheepjkeg/publish-review" target="_blank"> this</a> Chrome Extension,<br>
+      click on the icon<br>
+      then click "Proceed".</p>
+        <a href="https://chrome.google.com/webstore/detail/pojngholjbkbkblepogbkphpheepjkeg/publish-review" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/8/87/Google_Chrome_icon_%282011%29.png" width="70"></img><b> Chrome Extension</b></a>
+  </div>
+</div>
+
+<div class="w3-third">
+  <div class="w3-card w3-container" style="min-height:460px">
+  <h3>Google Contacts</h3><br>
+  <i class="fa fa-users w3-margin-bottom w3-text-theme" style="font-size:120px"></i>
+  <p>3) Login with your Google Account</p>
+  <p>4) Click "Search" to find all<br>
+    of the photo matches in your contacts</p>
+  <p>5) Click "Upload" to have the<br>
+    images sent to your Google Contacts.</p>
+  </div>
+</div>
+</div>
+<br>
+<br>
+
+<!-- Footer -->
+<!-- Footer -->
+<footer class="w3-container w3-theme-dark w3-padding-16">
+  <h3>Google Photos to Contacts</h3>
+</footer>
+
+</body>
+</html>
